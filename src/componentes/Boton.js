7aa002d68b8c2f21b2699627bf393c0ba2746154
Alejandro Bastidas <manuba2309@gmail.com>
2022-04-07 15:08:10 -0500

    creacion componente Boton

---
+++ src/componentes/Boton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Boton ({texto, es_boton_de_clic, manejar_clic}){
+    return(
+        <button 
+        className={es_boton_de_clic ? "boton_clic" : "boton_reiniciar"}
+        onClick={manejar_clic}>
+            {texto}
+        </button>
+    ) 
+}
+
+export default Boton;
